69f8f644c752ad276853c6fb3c0ca1a704649f55
Pranav Raj <rpranav955@gmail.com>
2025-06-19 18:01:59 +0530

    Create App.java

---
+++ Java-Programs/Alarm Clock/App.java
@@ -0,0 +1,31 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+class demo {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime = null;
+        String filePath = "Sizzr - Schwartzy.wav";
+
+        while (alarmTime == null) {
+            try {
+                System.out.print("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = sc.nextLine();
+
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for " + alarmTime);
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid Format, Please use (HH:MM:SS)");
+            }
+        }
+
+        AlarmClock alarmClock = new AlarmClock(alarmTime, filePath, sc);
+        Thread alarmThread = new Thread(alarmClock);
+        alarmThread.start();
+
+    }
+}
