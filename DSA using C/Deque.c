1678959f3605accdc6abc95e0ceb796b98d6bb7a
Pranav Raj <rpranav955@gmail.com>
2025-07-18 22:12:13 +0530

    Update Deque.c

--- DSA using C/Deque.c
+++ DSA using C/Deque.c
@@ -5,21 +5,53 @@
 
 int front = -1, rear = -1;
 
-void Enqueue(int queue[], int item){
+void frontEnqueue(int queue[], int item){
+    if(front == -1 && rear == -1){
+        rear =0;
+        queue[++front] = item;
+        printf("%d queued at the front\n", item);
+    } else if(rear == (front-1)%MAXSIZE){
+        printf("Queue Overflow\n");
+    } else {
+        front = (front-1)%MAXSIZE;
+        queue[front]=item;
+        printf("%d queued at the front\n", item);
+    }
+}
+
+void rearEnqueue(int queue[], int item){
     if(front == -1 && rear == -1){
         front =0;
         queue[++rear] = item;
-        printf("%d queued\n", item);
+        printf("%d queued at the rear\n", item);
     } else if(front == (rear+1)%MAXSIZE){
         printf("Queue Overflow\n");
     } else {
         rear = (rear+1)%MAXSIZE;
         queue[rear]=item;
-        printf("%d queued\n", item);
+        printf("%d queued at the rear\n", item);
     }
 }
 
-int Dequeue(int queue[]){
+int rearDequeue(int queue[]){
+    if(front == -1 && rear == -1){
+        printf("Queue Underflow\n");
+        return 0;
+    } else {
+        int item = queue[rear];
+        if(queue[rear] == queue[front]){
+            front = -1; rear = -1;
+            return item;
+        }
+        else{
+            rear = (rear-1)%MAXSIZE;
+            return item;
+        }
+        
+    }
+}
+
+int frontDequeue(int queue[]){
     if(front == -1 && rear == -1){
         printf("Queue Underflow\n");
         return 0;
@@ -39,8 +71,8 @@ int Dequeue(int queue[]){
     }
 }
 
-void printArray(int a[], int n){
-    for(int i =0; i<n; i++){
+void printArray(int a[], int front, int rear){
+    for(int i =front; i<rear+1; i++){
         printf("%d ", a[i]);
     }
     printf("\n");
@@ -48,94 +80,136 @@ void printArray(int a[], int n){
 
 int main(){
     
-    int queue[]= {0,0,0,0,0,0,0,0,0,0};
-    int choice2;
-    printf("Enter your choice:\n");
-    printf("1. Input Restricted Deque\n2. Output Restricted Deque\n");
-    scanf("%d", &choice2);
-    switch(choice2){
-        case 1: 
+    int queue[MAXSIZE];
+    int choice;
     
+    printf("1. Input Restricted Deque\n2. Output Restricted Deque\n3. No Restriction\n");
+    printf("Enter your choice (1/2/3): ");
+    scanf("%d", &choice);
     bool isEnd = false;
-
-    while(!isEnd){
+    
+    switch(choice){
+        case 1:
+        while(!isEnd){
         printf("Enter your choice:\n");
-        printf("1. Enqueue\n2. Dequeue\n3. Print Queue\n4. Exit\n\n");
-        int choice; 
-        scanf("%d",&choice);
+        printf("1. Input into rear\n2. Output from front\n3. Output from rear\n4. Print Queue\n5. Exit\n");
+        int choice1; 
+        scanf("%d",&choice1);
         printf("\n");
-        switch(choice){
+        switch(choice1){
             case 1: printf("Enter your element: ");
                     int item;
                     scanf("%d", &item);
-                    Enqueue(queue, item);
+                    rearEnqueue(queue, item);
                     break;
             
-            case 2: int n = Dequeue(queue);
-
-                    int r = queue[rear];
-                    queue[rear] = 0;
-                    rear = (rear - 1)%MAXSIZE;
-
-                    printf("%d, %d popped from front and rear\n", n, r);
-
+            case 2: int n1 = frontDequeue(queue);
+                    if(n1 != 0){
+                        printf("%d dequeued\n",n1);
+                    }
+                    break;
+                    
+            case 3: int n2 = rearDequeue(queue);
+                    if(n2 != 0){
+                        printf("%d dequeued\n",n2);
+                    }
                     break;
 
-            case 3: printArray(queue, MAXSIZE);
+            case 4: printArray(queue, front ,rear );
                     printf("\n");
                     break;
 
-            case 4: isEnd = true;
+            case 5: isEnd = true;
                     break;
             
             default: printf("Invalid Choice\n");
         }
     }
                 break;
+                
         case 2:
-
+        while(!isEnd){
         printf("Enter your choice:\n");
-        printf("1. Enqueue\n2. Dequeue\n3. Print Queue\n4. Exit\n\n");
-        int choice; 
-        scanf("%d",&choice);
+        printf("1. Input into front\n2. Input into rear\n3. Output from front\n4. Print Queue\n5. Exit\n");
+        int choice1; 
+        scanf("%d",&choice1);
         printf("\n");
-        switch(choice){
-            case 1: printf("Enter your element (front | rear): ");
-                    int item1, item2;
-                    scanf("%d%d", &item1, &item2);
-                    Enqueue(queue, item2);
-                    if(rear == (front -1 )%MAXSIZE){
-                        
-                    }
+        switch(choice1){
+            case 1: printf("Enter your element: ");
+                    int item2;
+                    scanf("%d", &item2);
+                    frontEnqueue(queue, item2);
                     break;
             
-            case 2: int n = Dequeue(queue);
-                    if(n != 0){
-                        printf("%d dequeued\n",n);
+            case 2: printf("Enter your element: ");
+                    int item3;
+                    scanf("%d", &item3);
+                    rearEnqueue(queue, item3);
+                    break;
+                    
+            case 3: int n3 = frontDequeue(queue);
+                    if(n3 != 0){
+                        printf("%d dequeued\n",n3);
                     }
                     break;
 
-            case 3: printArray(queue, MAXSIZE);
+            case 4: printArray(queue, front ,rear );
                     printf("\n");
                     break;
 
-            case 4: isEnd = true;
+            case 5: isEnd = true;
                     break;
             
             default: printf("Invalid Choice\n");
         }
-        
-        
-        
-        
-        break;
-        default:printf("Invalid Choice\n");
-                break;
     }
-    
-    
-    
-    
+                break;
+                
+        case 3:
+        while(!isEnd){
+        printf("Enter your choice:\n");
+        printf("1. Input into front\n2. Input into rear\n3. Output from front\n4. Output from rear\n5. Print Queue\n6. Exit\n");
+        int choice1; 
+        scanf("%d",&choice1);
+        printf("\n");
+        switch(choice1){
+            case 1: printf("Enter your element: ");
+                    int item4;
+                    scanf("%d", &item4);
+                    frontEnqueue(queue, item4);
+                    break;
+            
+            case 2: printf("Enter your element: ");
+                    int item5;
+                    scanf("%d", &item5);
+                    rearEnqueue(queue, item5);
+                    break;
+                    
+            case 3: int n4 = frontDequeue(queue);
+                    if(n4 != 0){
+                        printf("%d dequeued\n",n4);
+                    }
+                    break;
+                    
+            case 4: int n5 = rearDequeue(queue);
+                    if(n5 != 0){
+                        printf("%d dequeued\n",n5);
+                    }
+                    break;
+
+            case 5: printArray(queue, front ,rear );
+                    printf("\n");
+                    break;
+
+            case 6: isEnd = true;
+                    break;
+            
+            default: printf("Invalid Choice\n");
+        }
+    }
+                break;
+        default: printf("Invalid Choice");
+    }
     
     return 0;
 }
