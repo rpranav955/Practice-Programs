4a25e20c04d2fee345cdd25f0e310db3da8117f2
Pranav Raj <rpranav955@gmail.com>
2025-07-22 22:18:08 +0530

    Create Integer.R

---
+++ R Language/Integer.R
@@ -0,0 +1,9 @@
+num <- as.numeric(readline("Enter your number: "))
+
+if(num > 0){
+  cat("Your number is positive.\n")
+} else if(num < 0){
+  cat("Your number is negative.\n")
+} else {
+  cat("Your number is Zero\n")
+}
