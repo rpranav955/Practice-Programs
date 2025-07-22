42a0ff30e199e3af00cb20b0485c1867dec90ff9
Pranav Raj <rpranav955@gmail.com>
2025-07-22 22:48:02 +0530

    Create Average.R

---
+++ R Language/Average.R
@@ -0,0 +1,7 @@
+numbers <- c(12,34,56,78,90,23,45,67,89,100)
+
+cat("Input - List of Numbers:",paste(numbers, collapse=", "),"\n")
+
+average <- mean(numbers)
+
+cat("Output - Average of the numbers: ", average)
