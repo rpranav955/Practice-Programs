6e37d017158854d3f1094b89917104439ce79b8a
Pranav Raj <rpranav955@gmail.com>
2025-08-08 15:35:26 +0530

    Create PopulMean.R

---
+++ R Language/PopulMean.R
@@ -0,0 +1,15 @@
+population <- c(4779736,
+                701231,
+                6392017,
+                2915918,
+                37253956,
+                5029196,
+                3574097,
+                897934)
+mean <- mean(population)
+median <- median(population)
+variance <- var(population)
+
+cat("Mean of the population:", mean,"\n")
+cat("Median of the population:", median,"\n")
+cat("Variance of the population:", variance,"\n")
