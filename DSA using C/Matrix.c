43b9a2d0d43acd6baf8b32968368a4e03efda6b2
Pranav Raj <rpranav955@gmail.com>
2025-08-10 20:11:14 +0530

    Update Matrix.c

--- DSA using C/Matrix.c
+++ DSA using C/Matrix.c
@@ -1,20 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX 20
 
-typedef struct {
+typedef struct
+{
     int row;
     int col;
     int value;
-}Matrix;
+} Matrix;
 
-void display(Matrix a[], int n){
+void display(Matrix a[], int n)
+{
     printf("\nYour Matrix: \n");
-    for(int i = 0; i <= n; i++){
-        printf("%d  %d  %d\n", a[i].row, a[i].col, a[i].value);
+    printf("\nRow\tCol\tValue\n");
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d\t%d\t%d\n", a[i].row, a[i].col, a[i].value);
     }
 }
 
-int main(){
+Matrix *convert(int row, int col, int a[][col], int count)
+{
+
+    Matrix *new = (Matrix *)malloc((count + 1) * sizeof(Matrix));
+
+    if (new == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return NULL;
+    }
+
+    new[0].row = row;
+    new[0].col = row;
+    new[0].value = count;
+
+    int z = 1;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (a[i][j] != 0)
+            {
+                new[z].row = i;
+                new[z].col = j;
+                new[z++].value = a[i][j];
+            }
+        }
+    }
+
+    return new;
+}
+
+int main()
+{
 
     int row, col, count = 0;
 
@@ -25,33 +64,26 @@ int main(){
 
     int mat[row][col];
 
-    for(int i = 0; i < row; i++){
-        printf("Enter the elements of row %d: \n", i+1);
-        for(int j = 0; j < col; j++){
+    for (int i = 0; i < row; i++)
+    {
+        printf("Enter the elements of row %d: \n", i + 1);
+        for (int j = 0; j < col; j++)
+        {
             scanf("%d", &mat[i][j]);
-            if(mat[i][j] != 0){
+            if (mat[i][j] != 0)
+            {
                 count++;
             }
         }
     }
-    Matrix new[count + 1];
-    new[0].row = row; 
-    new[0].col = row; 
-    new[0].value = count; 
 
     // convert
-
-    int z= 1;
-
-    for(int i = 0; i < row; i++){
-        for(int j = 0; j < col; j++){
-            if(mat[i][j] != 0){
-                new[z].row = i;
-                new[z].col = j;
-                new[z++].value = mat[i][j];
-            }
-        }
+    Matrix *result = convert(row, col, mat, count);
+    if (result != NULL)
+    {
+        display(result, count);
+        free(result);
     }
-    display(new,count);
+
     return 0;
 }
