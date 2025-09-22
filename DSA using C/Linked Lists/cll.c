5d5cf973d82e122d5f45a015960627293f598e6f
Pranav Raj <rpranav955@gmail.com>
2025-09-22 22:24:31 +0530

    added cll

--- DSA using C/Linked Lists/cll.c
+++ DSA using C/Linked Lists/cll.c
@@ -1,11 +1,116 @@
-// CIRCULAR DOUBLY LINKED LISTS
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
+typedef struct node
+{
+    int data;
+    struct node *next;
+} Node;
+
+void printCLL(Node *header)
+{
+    if (!header)
+    {
+        printf("\nEmpty CLL\n");
+        return;
+    }
+    //  aaaaaaaaaaaaaaaaaaaaaaaaaaa
+    Node *ptr = header;
+    do
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    } while (ptr != header);
+    printf("\n");
+}
+
+void createNodeAtFront(Node **header)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (!newNode)
+    {
+        printf("Memory Allocation Failed\n");
+        return;
+    }
+
+    printf("Data: ");
+    scanf("%d", &newNode->data);
+
+    if (!*header)
+    {
+        *header = newNode;
+        newNode->next = *header;
+        return;
+    }
+
+    newNode->next = *header;
+    *header = newNode;
+}
+
 int main()
 {
+    Node *header = NULL;
+    bool isEnd = false;
+    int choice1, choice2;
+
+    printf("Circular Linked List\n");
+
+    while (!isEnd)
+    {
+        printf("\n1.\tCreate a node\n2.\tDelete a node\n3.\tPrint List\n4.\tStop\n\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice1);
+
+        switch (choice1)
+        {
+        case 1:
+            printf("1.\tAt Front\n2.\tAt End\n3.\tAfter a Position\n");
+            printf("Enter your choice: ");
+            scanf("%d", &choice2);
+
+            switch (choice2)
+            {
+            case 1:
+                createNodeAtFront(&header);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                printf("Invalid choice\n");
+            }
+
+            break;
+        case 2:
+            printf("1.\tFrom Front\n2.\tFrom End\n3.\tFrom a Position\n");
+            printf("Enter your choice: ");
+            scanf("%d", &choice2);
+
+            switch (choice2)
+            {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                printf("Invalid Choice\n");
+            }
+
+            break;
+        case 3:
+            printCLL(header);
+            break;
+        case 4:
+            isEnd = true;
+            break;
+        default:
+            printf("Invalid Choice\n");
+        }
+    }
 
     return 0;
 }
--- DSA using C/Linked Lists/webpage.c
+++ DSA using C/Linked Lists/webpage.c
@@ -5,64 +5,68 @@
 
 typedef struct node
 {
-    char data[500];
-    struct node *next;
-    struct node *prev;
+	char data[500];
+	struct node *next;
+	struct node *prev;
 } Node;
 
-int main(){
-	
+int main()
+{
+
 	Node page1, page2, page3;
 	bool isEnd = false;
 	char choice;
-	
+
 	strcpy(page1.data, "\nPage1\n");
 	page1.next = &page2;
 	page1.prev = NULL;
-	
+
 	strcpy(page2.data, "\nPage2\n");
 	page2.next = &page3;
 	page2.prev = &page1;
-	
+
 	strcpy(page3.data, "\nPage3\n");
 	page3.next = NULL;
 	page3.prev = &page2;
-	
+
 	Node *ptr = &page1;
-	
+
 	printf("%s", ptr->data);
-	
-			
+
 	printf("\npress f for forward\npress b for backward\npress e to exit\n");
-	
-	while(!isEnd){
-	
+
+	while (!isEnd)
+	{
+
 		scanf("%c", &choice);
-		
-		switch(choice){
-			
-			case 'f':	
-				if(!ptr->next){
-					printf("Theres no page ahead\n");
-					break;
-				}
-				ptr = ptr->next;
-				printf("%s", ptr->data);
+
+		switch (choice)
+		{
+
+		case 'f':
+			if (!ptr->next)
+			{
+				printf("Theres no page ahead\n");
 				break;
-				
-			case 'b':	
-				if(!ptr->prev){
-					printf("Theres no page behind\n");
-					break;
-				}
-				ptr = ptr->prev;
-				printf("%s", ptr->data);
+			}
+			ptr = ptr->next;
+			printf("%s", ptr->data);
+			break;
+
+		case 'b':
+			if (!ptr->prev)
+			{
+				printf("Theres no page behind\n");
 				break;
-			
-			case 'e':	
-				isEnd = true;
-				break;
+			}
+			ptr = ptr->prev;
+			printf("%s", ptr->data);
+			break;
+
+		case 'e':
+			isEnd = true;
+			break;
+		}
 	}
- }
 	return 0;
 }
