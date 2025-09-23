cb3f17f3fd96e1afea06aede9c7d08c40fdaecff
Pranav Raj <rpranav955@gmail.com>
2025-09-23 19:40:52 +0530

    completed cll

--- DSA using C/Linked Lists/cll.c
+++ DSA using C/Linked Lists/cll.c
@@ -15,7 +15,7 @@ void printCLL(Node *header)
         printf("\nEmpty CLL\n");
         return;
     }
-    //  aaaaaaaaaaaaaaaaaaaaaaaaaaa
+
     Node *ptr = header;
     do
     {
@@ -34,7 +34,7 @@ void createNodeAtFront(Node **header)
         return;
     }
 
-    printf("Data: ");
+    printf("\nData: ");
     scanf("%d", &newNode->data);
 
     if (!*header)
@@ -44,8 +44,170 @@ void createNodeAtFront(Node **header)
         return;
     }
 
+    Node *ptr = *header;
+
+    do
+    {
+        ptr = ptr->next;
+    } while (ptr->next != *header);
+
     newNode->next = *header;
     *header = newNode;
+    ptr->next = *header;
+}
+
+void createNodeAtEnd(Node **header)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (!newNode)
+    {
+        printf("Memory Allocation Failed\n");
+        return;
+    }
+
+    printf("\nData: ");
+    scanf("%d", &newNode->data);
+
+    if (!*header)
+    {
+        *header = newNode;
+        newNode->next = *header;
+        return;
+    }
+
+    Node *ptr = *header;
+
+    do
+    {
+        ptr = ptr->next;
+    } while (ptr->next != *header);
+
+    ptr->next = newNode;
+    newNode->next = *header;
+}
+
+void createNodeAfterPosition(Node **header)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (!newNode)
+    {
+        printf("Memory Allocation Failed\n");
+        return;
+    }
+
+    printf("\nData: ");
+    scanf("%d", &newNode->data);
+
+    int pos;
+    printf("Enter position: ");
+    scanf("%d", &pos);
+    if (pos < 1)
+    {
+        printf("\nInvalid Position\n");
+        return;
+    }
+
+    Node *ptr = *header;
+    while (pos > 1)
+    {
+        if (ptr->next == *header)
+        {
+            printf("\nInvalid Position\n");
+            return;
+        }
+        ptr = ptr->next;
+        pos--;
+    }
+
+    newNode->next = ptr->next;
+    ptr->next = newNode;
+}
+
+void delNodeFront(Node **header)
+{
+    if (!*header)
+    {
+        printf("Empty CLL\n");
+        return;
+    }
+
+    if ((*header)->next == *header)
+    {
+        free(*header);
+        *header = NULL;
+        return;
+    }
+
+    Node *ptr = *header;
+    do
+    {
+        ptr = ptr->next;
+    } while (ptr->next != *header);
+
+    ptr->next = (*header)->next;
+    free(*header);
+    *header = ptr->next;
+}
+
+void delNodeEnd(Node **header)
+{
+    if (!*header)
+    {
+        printf("Empty CLL\n");
+        return;
+    }
+
+    if ((*header)->next == *header)
+    {
+        free(*header);
+        *header = NULL;
+        return;
+    }
+
+    Node *ptr = *header, *prev;
+    do
+    {
+        prev = ptr;
+        ptr = ptr->next;
+    } while (ptr->next != *header);
+
+    free(ptr);
+    prev->next = *header;
+}
+
+void delNodeAtPos(Node **header)
+{
+    if (!*header)
+    {
+        printf("Empty CLL\n");
+        return;
+    }
+
+    int pos;
+    Node *ptr = *header, *prev;
+
+    printf("Enter position: ");
+    scanf("%d", &pos);
+    if (pos <= 1)
+    {
+        printf("\nInvalid Position\n");
+        return;
+    }
+
+    while (pos > 1)
+    {
+        if (ptr->next == *header)
+        {
+            printf("\nInvalid Position\n");
+            return;
+        }
+        prev = ptr;
+        ptr = ptr->next;
+        pos--;
+    }
+
+    prev->next = ptr->next;
+    free(ptr);
 }
 
 int main()
@@ -75,8 +237,10 @@ int main()
                 createNodeAtFront(&header);
                 break;
             case 2:
+                createNodeAtEnd(&header);
                 break;
             case 3:
+                createNodeAfterPosition(&header);
                 break;
             default:
                 printf("Invalid choice\n");
@@ -84,20 +248,31 @@ int main()
 
             break;
         case 2:
-            printf("1.\tFrom Front\n2.\tFrom End\n3.\tFrom a Position\n");
-            printf("Enter your choice: ");
-            scanf("%d", &choice2);
 
-            switch (choice2)
+            if (!header)
             {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            default:
-                printf("Invalid Choice\n");
+                printf("\nEmpty CLL\n");
+            }
+            else
+            {
+                printf("1.\tFrom Front\n2.\tFrom End\n3.\tFrom a Position\n");
+                printf("Enter your choice: ");
+                scanf("%d", &choice2);
+
+                switch (choice2)
+                {
+                case 1:
+                    delNodeFront(&header);
+                    break;
+                case 2:
+                    delNodeEnd(&header);
+                    break;
+                case 3:
+                    delNodeAtPos(&header);
+                    break;
+                default:
+                    printf("Invalid Choice\n");
+                }
             }
 
             break;
