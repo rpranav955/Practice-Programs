339ef07c0e457c488233b34bcbc4721aa9a394bf
Pranav Raj <rpranav955@gmail.com>
2025-10-10 15:04:28 +0530

    Update landmine.c

--- DSA using C/Graphs/landmine.c
+++ DSA using C/Graphs/landmine.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#define MAX 100
 #define INF 9999
+#define MAX 100
 
 int dirRow[] = {-1, 1, 0, 0};
 int dirCol[] = {0, 0, -1, 1};
@@ -14,6 +15,7 @@ struct Node
 
 struct Node queue[MAX * MAX];
 int front = 0, rear = -1;
+
 
 void enqueue(int x, int y)
 {
@@ -39,17 +41,18 @@ int main()
 
     printf("Enter number of rows and columns: ");
     scanf("%d%d", &n, &m);
-
-    printf("Enter the maze (-1 for mine, 0 for path, # for wall as 999):\n");
+    
+    printf("Enter the maze (-1 for mine, 0 for path, ....):\n");
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
             scanf("%d", &maze[i][j]);
-
-    for (int i = 0; i < n; i++)
+        // initialize
+       for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
             dist[i][j] = INF;
-
-    for (int i = 0; i < n; i++)
+           
+       // enqueue all the landmine 
+          for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
         {
             if (maze[i][j] == -1)
@@ -59,17 +62,23 @@ int main()
             }
         }
 
-    while (front <= rear)
+
+	while (front <= rear)
     {
         struct Node temp = dequeue();
         int x = temp.x;
         int y = temp.y;
 
         for (int k = 0; k < 4; k++)
-        {
+        {	
+        
+        		// changes into the new point (neighbour of the dequed point)
             int newX = x + dirRow[k];
             int newY = y + dirCol[k];
-
+				
+				// check whether the new point is outside the map or not
+				// and check whether the new point is not marked as a landmine inside the maze
+				// and check whether the new point is greater (first initialized as 999) than the current point
             if (isValid(newX, newY, n, m) && maze[newX][newY] == 0 && dist[newX][newY] > dist[x][y] + 1)
             {
                 dist[newX][newY] = dist[x][y] + 1;
@@ -77,8 +86,18 @@ int main()
             }
         }
     }
-
-    printf("Shortest distance from land mines:\n");
+    
+    printf("Your Maze:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf(" %d ", maze[i][j]);
+        }
+        printf("\n");
+    }
+	
+	printf("Shortest distance from land mines:\n");
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
